5b1e8f65ca21d1755c9dfdec671b257bd74be8ee
Viet Le <vietlq85@gmail.com>
2018-11-11 20:34:30 +0000

    added test cases for 128, 129 & 255

--- test/test.ml
+++ test/test.ml
@@ -10,11 +10,29 @@ let rlp_encode_char_1 _ =
 let rlp_encode_char_127 _ =
   assert_equal (Rlp.encode_char '\x7f') (Bytes.make 1 '\x7f')
 
+let rlp_encode_char_128 _ =
+  let bytes = Bytes.make 2 '\x81' in
+  Bytes.set bytes 1 '\x80';
+  assert_equal (Rlp.encode_char '\x80') bytes
+
+let rlp_encode_char_129 _ =
+  let bytes = Bytes.make 2 '\x81' in
+  Bytes.set bytes 1 '\x81';
+  assert_equal (Rlp.encode_char '\x81') bytes
+
+let rlp_encode_char_255 _ =
+  let bytes = Bytes.make 2 '\x81' in
+  Bytes.set bytes 1 '\xff';
+  assert_equal (Rlp.encode_char '\xff') bytes
+
 let suite =
   "Rlp" >::: [
     "rlp_encode_char_0" >:: rlp_encode_char_0;
     "rlp_encode_char_1" >:: rlp_encode_char_1;
     "rlp_encode_char_127" >:: rlp_encode_char_127;
+    "rlp_encode_char_128" >:: rlp_encode_char_128;
+    "rlp_encode_char_129" >:: rlp_encode_char_129;
+    "rlp_encode_char_255" >:: rlp_encode_char_255;
   ]
 
 let _ = run_test_tt_main suite
